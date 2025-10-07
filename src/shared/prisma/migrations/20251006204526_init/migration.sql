f5e4f15096754958490cb377229b73a92ee36599
Filimon4 <f.rykov@bk.ru>
2025-10-07 09:05:23 +0300

    changes

---
+++ src/shared/prisma/migrations/20251006204526_init/migration.sql
@@ -0,0 +1,126 @@
+-- CreateEnum
+CREATE TYPE "TransactionType" AS ENUM ('INCOME', 'EXPENSE');
+
+-- CreateEnum
+CREATE TYPE "OperationType" AS ENUM ('INCOME', 'EXPENSE', 'TRANSFER');
+
+-- CreateTable
+CREATE TABLE "account" (
+    "id" SERIAL NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "admin" BOOLEAN NOT NULL DEFAULT false,
+    "verified" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "account_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "bank_account" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "main" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "account_id" INTEGER NOT NULL,
+    "currency_id" INTEGER NOT NULL,
+
+    CONSTRAINT "bank_account_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "category" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "base_type" "TransactionType",
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "account_id" INTEGER NOT NULL,
+
+    CONSTRAINT "category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "currency" (
+    "id" SERIAL NOT NULL,
+    "symbol" TEXT NOT NULL,
+    "symbol_native" TEXT,
+    "code" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "currency_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "exchange_rate" (
+    "id" SERIAL NOT NULL,
+    "from_currency_id" INTEGER NOT NULL,
+    "to_currency_id" INTEGER NOT NULL,
+    "rate" DECIMAL(15,8) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "exchange_rate_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "operations" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT,
+    "category_id" INTEGER,
+    "account_id" INTEGER,
+    "cash_account_id" INTEGER NOT NULL,
+    "to_cash_account_id" INTEGER,
+    "exchange_rate" DECIMAL(15,2),
+    "amount" DECIMAL(15,2) NOT NULL,
+    "description" TEXT,
+    "type" "OperationType" NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "operations_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "bank_account_account_id_idx" ON "bank_account"("account_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "bank_account_name_account_id_key" ON "bank_account"("name", "account_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "category_name_account_id_key" ON "category"("name", "account_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "currency_symbol_key" ON "currency"("symbol");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "currency_symbol_native_key" ON "currency"("symbol_native");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "currency_code_key" ON "currency"("code");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "currency_name_key" ON "currency"("name");
+
+-- AddForeignKey
+ALTER TABLE "bank_account" ADD CONSTRAINT "bank_account_account_id_fkey" FOREIGN KEY ("account_id") REFERENCES "account"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "bank_account" ADD CONSTRAINT "bank_account_currency_id_fkey" FOREIGN KEY ("currency_id") REFERENCES "currency"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "category" ADD CONSTRAINT "category_account_id_fkey" FOREIGN KEY ("account_id") REFERENCES "account"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "exchange_rate" ADD CONSTRAINT "exchange_rate_from_currency_id_fkey" FOREIGN KEY ("from_currency_id") REFERENCES "currency"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "exchange_rate" ADD CONSTRAINT "exchange_rate_to_currency_id_fkey" FOREIGN KEY ("to_currency_id") REFERENCES "currency"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "operations" ADD CONSTRAINT "operations_cash_account_id_fkey" FOREIGN KEY ("cash_account_id") REFERENCES "bank_account"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "operations" ADD CONSTRAINT "operations_to_cash_account_id_fkey" FOREIGN KEY ("to_cash_account_id") REFERENCES "bank_account"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "operations" ADD CONSTRAINT "operations_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "category"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "operations" ADD CONSTRAINT "operations_account_id_fkey" FOREIGN KEY ("account_id") REFERENCES "account"("id") ON DELETE SET NULL ON UPDATE CASCADE;
